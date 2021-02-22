cac6ee8ba5418c4548a30f43da990f17bfbfd8f7
manmeetdevgun <manmeet.devgun@nagarro.com>
2021-02-22 19:13:43 +0530

    update

--- src/main/java/com/nagp/ucp/booking/controller/BookingController.java
+++ src/main/java/com/nagp/ucp/booking/controller/BookingController.java
@@ -1,5 +1,7 @@
 package com.nagp.ucp.booking.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/booking")
 public class BookingController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(BookingController.class);
 
 	@GetMapping(value = "/greet")
 	public String greet() {
